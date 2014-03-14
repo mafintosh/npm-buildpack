aef97a4cb0c6bf1871a11fde261f28f4ca473038
Mathias Buus <mathiasbuus@gmail.com>
2014-03-14 15:40:45 +0100

    workaround for npm prune issues

--- npm-buildpack.sh
+++ npm-buildpack.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-CACHE_ID="$1"
+run () {
+	[ "${ARGS[0]}" != "run" ] && return 0
+	npm "${ARGS[@]}"
+}
 
-if [ "$CACHE_ID" == "" ] && [ -f package.json ]; then
-	CACHE_ID=$(cat package.json | grep '"name"' | sed 's/"name"//; s/[^"]*"//; s/"[^"]*//')
-fi
+prune () {
+	# workaround for npm prune not working with bundled-deps
+	for mod in "$(ls node_modules)"; do
+		[ -d "node_modules/$mod/node_modules" ] && mv "node_modules/$mod/node_modules" "node_modules/$mod/node_modules.bak"
+	done
+	npm prune > /dev/null 2> /dev/null
+	for mod in "$(ls node_modules)"; do
+		[ -d "node_modules/$mod/node_modules.bak" ] && mv "node_modules/$mod/node_modules.bak" "node_modules/$mod/node_modules"
+	done
+}
+
+ARGS=("$@")
+CACHE_ID=$(cat package.json | grep '"name"' | sed 's/"name"//; s/[^"]*"//; s/"[^"]*//')
 
 if [ "$CACHE_ID" == "" ]; then
 	CACHE_ID=__misc__
@@ -14,11 +27,14 @@ CACHE_DIR="/tmp/npm-buildpack/$CACHE_ID"
 
 if [ -d node_modules ]; then
 	npm rebuild
+	run
 else
 	mkdir -p "$CACHE_DIR"
 	cp -r "$CACHE_DIR" node_modules
-	npm prune > /dev/null 2> /dev/null
-	npm install --production || exit $?
+	prune
+	npm install || exit $?
 	rm -rf "$CACHE_DIR"
 	cp -r node_modules "$CACHE_DIR"
+	run || exit $?
+	npm prune --production > /dev/null 2> /dev/null
 fi
