8b3c6a264482a8e372d32baf2e0171250874f83e
Mathias Buus <mathiasbuus@gmail.com>
2014-12-09 10:36:35 -0800

    resupport --help and fix build on newer npm. fixes #1

--- npm-buildpack.sh
+++ npm-buildpack.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+if [ "$1" == "--help" ]; then
+  echo "Usage: npm-buildpack [npm-options]"
+  echo ""
+  echo "  [npm-options] are passed to npm install"
+  echo ""
+  exit 0
+fi
+
+build () {
+  grep '"build":' package.json 2>/dev/null >/dev/null && npm run build
+}
+
 if [ -d node_modules ]; then
-  npm rebuild
-  npm run build
+  npm rebuild "$@"
+  build
 else
-  npm install --cache-min 99999999
-  npm run build
+  npm install --cache-min 99999999 "$@"
+  build
 fi
